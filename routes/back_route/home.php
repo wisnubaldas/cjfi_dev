bed2d48c09b08a7bb78c1cb236eb070b82593e73
arfa707 <arfabima1986@gmail.com>
2021-10-07 00:30:26 +0700

    pertamina

--- routes/back_route/home.php
+++ routes/back_route/home.php
@@ -3,6 +3,4 @@ use App\Http\Controllers\Backend\HomeController;
 
 Route::prefix('home')->group(function(){
     Route::get('/',[HomeController::class,'index']);
-    Route::get('/slot-mobil',[HomeController::class,'slot_kosong']);
-    Route::get('/status-boking',[HomeController::class,'status_boking']);
 });

b2131ea499ad079714d801a2b450426f083b07fa
wisnu Baldas <wisnubaldas@MacBook-Air-wisnu.local>
2022-02-21 20:30:30 +0700

    setu db again

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'remember_token' => '49A25F9FEEFAFFECAD0FCD30C50DC9331CFF8B55ECE53DEF6285C09E17E6F5D7',
         ]);
         \App\Models\User::factory(10)->create();
         // \App\Models\Brand::factory(40)->create();
